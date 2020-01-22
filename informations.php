b29c354bd07364476ca88798eaa02375f6ed2158
alham-khoshal <60146449+alham-khoshal@users.noreply.github.com>
2020-01-22 11:11:15 +0100

    Add files via upload

---
+++ informations.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<html>
+<head>
+	<?php include("head.html");?>
+
+
+</head>
+<body>
+	<?php include("image.html");?>
+	<?php include("nav.html");?>
+	<?php include("informations.html");?>
+	<?php include("footer.html");?>
+	<?php include("video.html");?>
+</body>
+</html>
