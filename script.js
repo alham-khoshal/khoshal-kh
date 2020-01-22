be2d0f956432c0e1958509af7ff9c5abf1e08044
alham-khoshal <60146449+alham-khoshal@users.noreply.github.com>
2020-01-22 11:13:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+let varaible2 = ["hello","i","am","happy","la"];
+let varaible1= ["hello",varaible2];
+let varaible3=varaible1[1];
+let bool=true;
+let nb1="12";
+let nb2="20";
+let nb3= nb1+nb2;
+alairt("ghjyjy")
+let saisise=pormpt
+
+
+let age= 30;
+
+
+
+  if ( age =>20 && age <=25) {
+    alert("250€");}
+   else if(age =>26 && age <=29) {
+    alert("300€")  ;
+  }
+  
+else if(age =30){
+    alert("super");
+  }
+  
+  else if(age >65){
+    alert("400€")  ;
+  }
+  
+  
